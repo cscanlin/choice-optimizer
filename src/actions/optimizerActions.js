5f5b961634f98fa141b2a47d063f39a1e1ec8dc2
Chris Scanlin <cscanlin@gmail.com>
2017-02-13 22:53:47 -0800

    fix actions eslint issues

--- src/actions/optimizerActions.js
+++ src/actions/optimizerActions.js
@@ -1,80 +1,59 @@
 import * as types from './actionTypes'
 
-export const addName = () => {
-  return {
-    type: types.ADD_NAME,
-  }
-}
+export const addName = () => ({
+  type: types.ADD_NAME,
+})
 
-export const updateName = (cellID, newValue) => {
-  return {
-    type: types.UPDATE_NAME,
-    cellID,
-    newValue,
-  }
-}
+export const updateName = (cellID, newValue) => ({
+  type: types.UPDATE_NAME,
+  cellID,
+  newValue,
+})
 
-export const addChoice = () => {
-  return {
-    type: types.ADD_CHOICE,
-  }
-}
+export const addChoice = () => ({
+  type: types.ADD_CHOICE,
+})
 
-export const updateChoice = (cellID, newValue) => {
-  return {
-    type: types.UPDATE_CHOICE,
-    cellID,
-    newValue,
-  }
-}
+export const updateChoice = (cellID, newValue) => ({
+  type: types.UPDATE_CHOICE,
+  cellID,
+  newValue,
+})
 
-export const updateRank = (cellID, newValue) => {
-  return {
-    type: types.UPDATE_RANK,
-    cellID,
-    newValue,
-  }
-}
+export const updateRank = (cellID, newValue) => ({
+  type: types.UPDATE_RANK,
+  cellID,
+  newValue,
+})
 
-export const updateMaxPerChoice = (cellID, newValue) => {
-  return {
-    type: types.UPDATE_MAX_PER_CHOICE,
-    cellID,
-    newValue,
-  }
-}
+export const updateMaxPerChoice = (cellID, newValue) => ({
+  type: types.UPDATE_MAX_PER_CHOICE,
+  cellID,
+  newValue,
+})
 
-export const updateChoicesPerName = (cellID, newValue) => {
-  return {
-    type: types.UPDATE_CHOICES_PER_NAME,
-    cellID,
-    newValue,
-  }
-}
+export const updateChoicesPerName = (cellID, newValue) => ({
+  type: types.UPDATE_CHOICES_PER_NAME,
+  cellID,
+  newValue,
+})
 
-export const updateNoRepeatChoices = (newValue) => {
-  return {
-    type: types.UPDATE_NO_REPEAT_CHOICES,
-    newValue,
-  }
-}
+export const updateNoRepeatChoices = newValue => ({
+  type: types.UPDATE_NO_REPEAT_CHOICES,
+  newValue,
+})
 
-export const requestScores = () => {
-  return {
-    type: types.REQUEST_SCORES,
-  }
-}
+export const requestScores = () => ({
+  type: types.REQUEST_SCORES,
+})
 
-export const receiveScores = (scores) => {
-  return {
-    type: types.RECEIVE_SCORES,
-    scores,
-    receivedAt: Date.now(),
-  }
-}
+export const receiveScores = scores => ({
+  type: types.RECEIVE_SCORES,
+  scores,
+  receivedAt: Date.now(),
+})
 
 export const fetchScores = (bodyData) => {
-  console.log(bodyData)
   return (dispatch) => {
     dispatch(requestScores(bodyData))
     return fetch('/optimize_choices', {
