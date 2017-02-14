10453fb70bc54bb4e48dea88517b7944ab995d19
Chris Scanlin <cscanlin@gmail.com>
2017-02-13 17:44:27 -0800

    initial redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './App'
+import configureStore from './store/configure-store'
 import './index.css'
 
+const store = configureStore()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import people from './optimizer-reducer'
+
+const rootReducer = combineReducers({
+  people,
+})
+
+export default rootReducer
