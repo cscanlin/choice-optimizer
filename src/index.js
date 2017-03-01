359b41a3d41e8d65c6c507d55f54c765597f782a
Chris Scanlin <cscanlin@gmail.com>
2017-03-01 01:42:40 -0800

    add github corner banner

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import OptimizerApp from './containers/OptimizerApp'
 import configureStore from './store/configureStore'
-import './css/index.css'
 import './css/OptimizerApp.css'
 import './css/reactDropdown.css'
+import './css/githubCorner.css'
 
 const store = configureStore()
 
