0a28fc29e300cf689cbadcb97bbcfebefdec0267
Chris Scanlin <cscanlin@gmail.com>
2017-02-28 18:02:45 -0800

    semi working lambda handler

--- choice_optimizer_lambda/deploy_lambda.sh
+++ choice_optimizer_lambda/deploy_lambda.sh
@@ -1,11 +1,14 @@
 deploy () {
     cp choice_optimizer_deps.zip choice_optimizer_latest.zip
     zip choice_optimizer_latest.zip handlers.py choice_optimizer.py
+    echo "zipped!"
     aws s3 cp choice_optimizer_latest.zip s3://cscanlin-choice-optimizer/
+    echo "uploaded!"
     aws lambda update-function-code \
       --function-name choice-optimizer \
       --s3-bucket cscanlin-choice-optimizer \
       --s3-key choice_optimizer_latest.zip
+    echo "finished!"
 }
 
 deploy
--- choice_optimizer_lambda/handlers.py
+++ choice_optimizer_lambda/handlers.py
@@ -6,7 +6,7 @@ import sys
 LIBS = os.path.join(os.getcwd(), 'lib')
 
 def handler(filename):
-    def handle(event, context):
+    def handle(event, context=None):
         env = os.environ.copy()
         env.update(LD_LIBRARY_PATH=LIBS)
         proc = subprocess.Popen(
@@ -17,13 +17,19 @@ def handler(filename):
             stderr=subprocess.STDOUT)
         (stdout, _) = proc.communicate(input=json.dumps(event))
         try:
-            return json.loads(stdout)
+            # return json.loads(stdout)
+            return {
+                'statusCode': 200,
+                'headers': {},
+                'body': json.loads(stdout),
+            }
         except ValueError:
             raise ValueError(stdout)
     return handle
 
 def invoking(f):
-    output = f(json.load(sys.stdin))
+    # output = f(json.load(sys.stdin))
+    output = f(json.loads(json.load(sys.stdin)['body']))
     json.dump(output, sys.stdout)
 
 choice_optimizer = handler('choice_optimizer.py')
